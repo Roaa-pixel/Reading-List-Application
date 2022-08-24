f84523cae785c97b94e4f9e76f314d03d454bf91
Roaa Zaki <72927350+Roaa-pixel@users.noreply.github.com>
2022-08-24 22:20:18 +0200

    Add files via upload

---
+++ v2.0.0/pluralsight-projects-Vue-BookListing-e970e5c/src/components/BookList.vue
@@ -0,0 +1,59 @@
+<template><div>
+
+    <h1>{{title}}</h1>
+    <ul><book-item v-for="book in books" :book="book"></book-item></ul>
+    <book-form @addBook='appendBook'></book-form>
+</div></template>
+<script>
+import BookItem from './BookItem';
+import BookForm from './BookForm';
+export default
+{
+    name: 'Booklist',
+    title: 'All Books',
+    
+    data(){
+        return{
+            title,
+            book:{ books: [
+
+    {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+    {title: 'American Spark', author: 'Neil Gaiman'},
+    {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+]
+        
+        },
+        
+}
+    
+},
+components: {
+    BookItem,
+    BookForm,
+  },
+  methods: {
+    appendBook(bookTitle, bookAuthor) {
+      this.books.push({ title: bookTitle, author: bookAuthor });
+    },
+},
+};
+</script>
+<style>
+h1,h2
+{
+
+font-weight: normal;
+}
+ul
+{
+list-style-type:none;
+padding:0;
+}
+li
+{
+display:block;
+margin:0 10px;
+}
+
+
+</style>
